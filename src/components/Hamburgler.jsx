9aec558ea67d4390d6e51fbd0f6d431cd9388ea7
Maggie Chew <maggie.chew.89@gmail.com>
2022-11-26 12:34:41 -0700

    Hamburgler Css Org/ Tweaks

--- src/components/Hamburgler.jsx
+++ src/components/Hamburgler.jsx
@@ -1,23 +1,23 @@
 import React from "react";
+import './Hamburgler.css'
 
 const Hamburgler = (props) => {
-  const setMenuOpen = props.setMenuOpen;
+  const setMenuClosed = props.setMenuClosed;
   return (
-    // <div className="hamburgler_container">
-    <div>
-      <input type="checkbox" name="menu" id="menu" />
-      <label for="menu">
-        <div onClick={() => setMenuOpen((curr) => !curr)}>
+    <div className="hamburgler_container">
+      <input className='menu' type="checkbox" name="menu" id="menu" />
+      <label className='hamburgler-label' htmlFor="menu">
+        <div onClick={() => setMenuClosed((curr) => !curr)}>
           <svg
             className="hamburgler"
             viewBox="0 0 500 500"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path class="top" d="M0 0H500V93H0V0Z" fill="black" />
-            <path class="middle1" d="M0 204H500V297H0V204Z" fill="darkgrey" />
-            <path class="middle2" d="M0 204H500V297H0V204Z" fill="hotpink" />
-            <path class="bottom" d="M0 407H500V500H0V407Z" fill="black" />
+            <path className="top" d="M0 0H500V93H0V0Z"  />
+            <path className="middle1" d="M0 204H500V297H0V204Z"  />
+            <path className="middle2" d="M0 204H500V297H0V204Z"  />
+            <path className="bottom" d="M0 407H500V500H0V407Z"  />
           </svg>
         </div>
       </label>
