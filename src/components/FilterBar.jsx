77a0aa6a96c84096136fea4a75b933f2d46b121d
Maggie Chew <maggie.chew.89@gmail.com>
2022-11-27 23:17:24 -0700

    FilterBar Work

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+// import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+import "./FilterBar.css";
 
 function FilterBar(props) {
-    const {crimeFilters, setCrimeFilters, timeFilters, setTimeFilters} = props;
-    const potentialCrimes = [];
-    const potentialTimes = [];
-
+  const { menuOpen, setMenuOpen } = props;
+  const [value, setValue] = useState(0);
+  // const [menuClosed, setMenuClosed] = useState(false);
 
   return (
-    <div>FilterBar</div>
-  )
+    <Box sx={{ boxShadow: 1 }} id="filterbar" variant="outlined" onClick={() => setMenuOpen((curr) => !curr)}>
+     <BottomNavigation sx={[{width: 350}, {pb: 1}]} position="static"
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+
+{menuOpen &&
+  <BottomNavigationAction label="Hide Map Menu" icon={<KeyboardArrowDownIcon />} />
+}
+{!menuOpen &&
+        <BottomNavigationAction label="Show Map Menu" icon={<KeyboardArrowUpIcon />} /> }
+        
+      </BottomNavigation>
+      {/* <AppBar id="appbar" position="static">
+        {menuOpen &&
+        <> */}
+          {/* <Typography color="black">
+          Lizard
+        </Typography> */}
+         {/* <KeyboardArrowDownIcon class="arrow-button" />
+         </>}
+        {!menuOpen && <KeyboardArrowUpIcon class="arrow-button" />}
+      </AppBar> */}
+    </Box>
+  );
 }
 
-export default FilterBar
+export default FilterBar;
