9ad589b01f51a8295a3d7083fdd66779d14d4f88
Maggie Chew <maggie.chew.89@gmail.com>
2022-11-27 23:11:07 -0700

    MapPage updates

--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import FilterBar from './FilterBar';
+import FilterMenu from './FilterMenu';
 import Map from './Map';
 import './MapPage.css'
+//TODO: animations?? transitions??
 
 function MapPage(props) {
   const {crimeFilters, timeFilters } = props;
+  const [menuOpen, setMenuOpen] = useState(false);
+
 
   return (
     <div className='map-page'>
     <Map crimeFilters={crimeFilters} timeFilters={timeFilters} />
-    <FilterBar {...props} />
+    <FilterBar className= 'filter-bar'setMenuOpen={setMenuOpen} menuOpen={menuOpen} />
+    {menuOpen &&
+    <FilterMenu id= 'filter-menu' {...props} />
+    }
     </div>
   )
 }
