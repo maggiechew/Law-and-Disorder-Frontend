8c84c7ca5117b7261dc49283ed8a02155e7cd891
Maggie Chew <maggie.chew.89@gmail.com>
2022-12-01 01:00:32 -0700

    About Project Page

--- src/components/AboutProject.jsx
+++ src/components/AboutProject.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 import logo from "../Law&DisOrderLogoV11_Horizontal.png";
 
@@ -14,9 +15,10 @@ function AboutProject() {
           <Typography variant="h4" gutterbottom>
             Our Mission
             <hr />
-          </Typography>
+          </Typography>{" "}
+          <br />
           <Typography variant="body1" sx={{ pt: 2, fontSize: 20 }}>
-            At Law & Disorder, We are inspired to make meaningful change in our
+            At Law & Disorder, we are inspired to make meaningful change in our
             city by bringing awareness to key issues as they progress each year,
             and eventually be able to give us a more accurate idea of how we can
             tackle crime in a new innovative way with the power of community
@@ -31,20 +33,68 @@ function AboutProject() {
             How It Works
             <hr />
           </Typography>
-          <Typography variant="body1"  sx={{ pt: 2, fontSize: 20, textDecoration:'none'}}>
-              
-            <span></span>
+          <br />
+          <Typography
+            variant="body1"
+            sx={{ pt: 2, fontSize: 20, textDecoration: "none" }}
+          >
             Data collected by the City of Calgary is collected monthly and
             stored on their publicly-accessible&nbsp;
             <a
               href="https://data.calgary.ca/Health-and-Safety/Community-Crime-Statistics/78gh-n26t/data"
               target="_blank"
               rel="noreferrer"
-              className="krang"
+              // className="krang"
             >
-              website 
+              website
             </a>
-            . )
+            .<br />
+            We are able to access this data, thanks to&nbsp;
+            <a
+              href="https://dev.socrata.com/foundry/data.calgary.ca/78gh-n26t"
+              target="_blank"
+              rel="noreferrer"
+              // className="krang"
+            >
+              Socrata Open Data
+            </a>
+            , which allows us to fetch from their SODA API using "SoQL queries",
+            in which our requests can use SQL-like queries to fetch the
+            information we need.
+          </Typography>
+          <br />
+          <Box sx={{ textAlign: "center", pt: 0, pb: 2, boxShadow: 4 }}>
+            <Typography
+              variant="body1"
+              sx={{
+                fontSize: 20,
+                fontWeight: "bold",
+                textDecoration: "none",
+                pb: 1,
+              }}
+            >
+              Example Query:
+            </Typography>
+            <Typography variant="body2" sx={{ fontSize: 18 }}>
+              https://data.calgary.ca/resource/78gh-n26t.json?$where=crime_count&nbsp;
+              {">"} 1
+            </Typography>
+          </Box>
+          <Typography
+            variant="body1"
+            sx={{ pt: 2, fontSize: 20, textDecoration: "none" }}
+          >
+            We, in turn, interpret this data in order to help users gain
+            meaningful insights through what we hope is a highly-accessible user
+            interface.
+            <br /> We have used MapBox to provide highly visual feedback about
+            the density of crime in all of Calgary's neighborhoods; the user
+            input of Crime Filters will allow them to see "hot spots" in their
+            communities, where all, some, or even only one crime type is more
+            prevalent. Users can even limit their queries to specific seasons;
+            this might be particularly useful to tourists visiting the city, or
+            new arrivals moving here.<br />By allowing users to increase the
+            weighting of certain crimes, we believe that they will be able to make their own decisions about what types of crime matter to them, and then make better-informed decisions about their and their loved ones' safety.
           </Typography>
         </div>
       </div>
