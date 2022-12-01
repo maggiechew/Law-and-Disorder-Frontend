67cf40f16100f26034c9522663fa0078669f3731
Maggie Chew <maggie.chew.89@gmail.com>
2022-11-30 22:26:32 -0700

    About Us stuff

---
+++ src/components/AboutProject.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Typography from "@mui/material/Typography";
+
+import logo from '../Law&DisOrderLogoV11_Horizontal.png';
+
+
+function AboutProject() {
+  return (
+    <div className="frank2">
+    <img src= {logo} alt='Law-and-Disorder-Logo'/>
+    <div className='test'>
+    <Typography variant='h4'>
+      Hi
+    </Typography>
+    </div>
+    
+    
+    </div>
+  )
+}
+
+export default AboutProject
--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import Typography from "@mui/material/Typography";
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  NavLink,
+} from "react-router-dom";
 import BioCards from "./BioCards";
 
-
 import "./AboutUs.css";
+import AboutProject from "./AboutProject";
+import AboutHeader from "./AboutHeader";
+
 
 function AboutUs() {
+  const [aboutProject, setAboutProject] = useState(false)
   return (
     <div className="about-us">
-      <div className="header">
-        <Typography variant="h2">About Us</Typography>
-        <p>Learn more about us!</p>
-      </div>
-      <BioCards />
+    <AboutHeader setAboutProject={setAboutProject} aboutProject={aboutProject}/>
+     {aboutProject &&
+     <AboutProject />
+     }
+     {!aboutProject &&
+     <BioCards />
+     }
+     
+      {/* <BioCards /> */}
     </div>
   );
 }
