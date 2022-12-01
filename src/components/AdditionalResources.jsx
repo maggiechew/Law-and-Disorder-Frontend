783040a91ecb76a26e87127ac86ce287832e3485
Maggie Chew <maggie.chew.89@gmail.com>
2022-12-01 11:00:40 -0700

    Resources

--- src/components/AdditionalResources.jsx
+++ src/components/AdditionalResources.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import ResourcesHeader from "./ResourcesHeader";
+import "./Resources.css";
 
 function AdditionalResources() {
   return (
-    <div>AdditionalResources</div>
-  )
+    <div>
+      <ResourcesHeader />
+      <div className="krang">
+        <div className="resource-content">
+        <TableContainer component={Paper}>
+          <Table className="table-style">
+            <TableHead>
+              <TableRow>
+                <TableCell>The table header</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            <TableRow>
+                <TableCell>The table body</TableCell>   
+              </TableRow>
+            <TableRow>
+            <TableCell>with two columns</TableCell>                
+              </TableRow>
+            </TableBody>
+          </Table>
+          </TableContainer>
+        </div>
+    </div>
+    </div>
+  );
 }
 
-export default AdditionalResources
+export default AdditionalResources;
---
+++ src/components/ResourcesHeader.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+function ResourcesHeader() {
+  return (
+    <div className="resources">
+      <div className="resource-header">
+        <div className="page-title">
+          <Typography variant="h3" gutterbottom>
+            About This Project
+          </Typography>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ResourcesHeader;
