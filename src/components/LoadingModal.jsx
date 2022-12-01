ef390744ee8afd8619a4df71c7752a36c70af157
Jared Huddleston <jaredhud@hotmail.com>
2022-12-01 12:13:35 -0700

    Set default profiles

--- src/components/LoadingModal.jsx
+++ src/components/LoadingModal.jsx
@@ -18,19 +18,79 @@ import FormLabel from "@mui/material/FormLabel";
 import logo from "../Law&DisOrderLogoV11_Horizontal.png";
 
 export default function LoadingModal(props) {
-const {setTimeFilters, setWeights} = props;
+  const { setTimeFilters, setWeights } = props;
   const [open, setOpen] = React.useState(true);
+  const [value, setValue] = React.useState("tourist");
+  const touristWeight = {
+    assault: 7,
+    bneStore: 0,
+    bneHome: 9,
+    bneOther: 0,
+    robStreet: 10,
+    robStore: 0,
+    violence: 10,
+    robOfCar: 5,
+    robFromCar: 5,
+  };
+  const touristTime = ["summer", "fall"];
+  const homebuyerWeight = {
+    assault: 7,
+    bneStore: 3,
+    bneHome: 10,
+    bneOther: 7,
+    robStreet: 3,
+    robStore: 2,
+    violence: 5,
+    robOfCar: 8,
+    robFromCar: 8,
+  };
+  const homebuyerTime = ["fall", "summer", "spring", "winter"];
+  const communityMemberWeight = {
+    assault: 6,
+    bneStore: 7,
+    bneHome: 8,
+    bneOther: 6,
+    robStreet: 8,
+    robStore: 8,
+    violence: 7,
+    robOfCar: 8,
+    robFromCar: 8,
+  };
+  const communityMemberTime = ["fall", "summer", "spring", "winter"];
   // const blurLoading = props.blurLoading;
   // const handleClose = () => {
   //   setOpen(false);
   // };
-  const handleToggle = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
     setOpen(!open);
+    console.log(value);
+    switch (value) {
+      case "tourist":
+        setWeights(touristWeight);
+        setTimeFilters(touristTime);
+        localStorage.setItem("weights", JSON.stringify(touristWeight));
+        localStorage.setItem("times", touristTime);
+        break;
+      case "homebuyer":
+        setWeights(homebuyerWeight);
+        setTimeFilters(homebuyerTime);
+        localStorage.setItem("weights", JSON.stringify(homebuyerWeight));
+        localStorage.setItem("times", homebuyerTime);
+        break;
+      case "communityMember":
+        setWeights(communityMemberWeight);
+        setTimeFilters(communityMemberTime);
+        localStorage.setItem("weights", JSON.stringify(communityMemberWeight));
+        localStorage.setItem("times", communityMemberTime);
+        break;
+      default:
+        break;
+    }
   };
-
-const handleProfiles = (event) => {
-
-}
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
   return (
     <Backdrop
@@ -56,8 +116,11 @@ const handleProfiles = (event) => {
             </FormLabel>
             <RadioGroup
               aria-labelledby="demo-radio-buttons-group-label"
-              defaultValue="female"
-              name="radio-buttons-group"
+              defaultValue="tourist"
+              name="profile_select"
+              id="profile_select"
+              value={value}
+              onChange={handleChange}
             >
               <FormControlLabel
                 value="communityMember"
@@ -70,7 +133,7 @@ const handleProfiles = (event) => {
                 label="Tourist"
               />
               <FormControlLabel
-                value="Homebuyer"
+                value="homebuyer"
                 control={<Radio />}
                 label="Homebuyer"
               />
@@ -93,7 +156,7 @@ const handleProfiles = (event) => {
             </Typography> */}
         </CardContent>
         <CardActions>
-          <Button size="small" onClick={handleToggle}>
+          <Button type="submit" size="small" onClick={handleSubmit}>
             Submit
           </Button>
         </CardActions>
